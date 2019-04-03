f67385eb6ffefce75088f0c6e7aa227d504d44dc
TomCass <qwerto227@gmail.com>
2019-04-03 01:35:09 +0100

    Added populateTables and Findalignment

--- Question 1.py
+++ Question 1.py
@@ -12,14 +12,16 @@ indel=-2
 # YOUR FUNCTIONS GO HERE -------------------------------------
 # 1. Populate the scoring matrix and the backtracking matrix
 def initTable(firstseq, secondseq):
-    table=[]*(len(firstseq)+2)
-    for i in table:
+    table=["UNSET"]*(len(firstseq)+2)
+    for i in range(len(table)):
         table[i]=[""]*(len(secondseq)+2)
         if i>=2:
             table[i][0]=firstseq[i-2]
-    for i in table[0]:
+    for i in range(len(secondseq)+2):
+
         if i>=2:
             table[0][i]=secondseq[i-2]
+    print(table)
     return table
 
 def checkint(inp): #returns inp if integer, otherwise returns -1
@@ -32,13 +34,13 @@ def matchscore(x,y, scoretable):
     xval=scoretable[x][0]
     yval=scoretable[0][y]
     if xval==yval:
-        if xval="A":
+        if xval=="A":
             return matchA
-        if xval="C":
+        if xval=="C":
             return matchC
-        if xval="G":
+        if xval=="G":
             return matchG
-        if xval="T":
+        if xval=="T":
             return matchT
     else:
         return mismatch
@@ -55,11 +57,36 @@ def populateTables(scoretable,backtrack):
                 diag=scoretable[x-1][y-1]+matchscore(x,y,scoretable)
                 scoretable[x][y]=max(top,left,diag, key=checkint) #OPTIMISING: MAYBE REMOVE KEY AND CHECK IN IF STATEMENTS BELOW
                 if scoretable[x][y]==top:
-                    backtrack[x][y].append("U")
+                    backtrack[x][y].append("U") #MAYBE NEEDS TO BE =backtrack[x][y]+"U"
                 if scoretable[x][y]==left:
                     backtrack[x][y].append("L")
                 if scoretable[x][y]==diag:
                     backtrack[x][y].append("D")
+
+def findAlignment(backtrack):
+    xlen=len(backtrack) #Length of table in x axis
+    ylen=len(backtrack[0]) #Length of table in y axis
+
+    currpoint=[xlen-1, ylen-1]
+    alignment = [""]*2
+    while currpoint!=[1,1]:
+        xval = scoretable[currpoint[0]][0]
+        yval = scoretable[0][currpoint[1]]
+        currval=backtrack[currpoint[0]][currpoint[1]]
+        if "D" in currval:
+            currpoint=[currpoint[0]-1,currpoint[1]-1]
+            alignment[0]=xval+alignment[0]
+            alignment[1]=yval+alignment[1]
+        elif "U" in currval:
+            currpoint=[currpoint[0],currpoint[1]-1]
+            alignment[0]="-"+alignment[0]
+            alignment[1]=yval+alignment[1]
+        elif "L" in currval:
+            currpoint=[currpoint[0]-1,currpoint[1]]
+            alignment[0]=xval+alignment[0]
+            alignment[1]="-"+alignment[1]
+        return alignment
+
 
 # ------------------------------------------------------------
 
@@ -101,7 +128,9 @@ start = time.time()
 # YOUR CODE GOES HERE ----------------------------------------
 scoretable=initTable(seq1,seq2)
 backtrack=scoretable.copy()
-
+populateTables(scoretable,backtrack)
+best_score=scoretable[len(scoretable)-1][len(scoretable[0])-1]
+best_alignment=findAlignment(backtrack)
 
 # The sequences are contained in the variables seq1 and seq2 from the code above.
 # Intialise the scoring matrix and backtracking matrix and call the function to populate them
